aedb5323c62e53e5b2bc3cb54c4a086af9764c6c
Amar Dixit <86550850+amardixit94@users.noreply.github.com>
2022-08-17 09:04:50 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,6 @@
 import firebase from "firebase";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDXO40ZHxKgYNwDnlh9rfFRyL_bvrr7-RY",
-  authDomain: "disneyplus-clone-cf354.firebaseapp.com",
-  projectId: "disneyplus-clone-cf354",
-  storageBucket: "disneyplus-clone-cf354.appspot.com",
-  messagingSenderId: "817671313986",
-  appId: "1:817671313986:web:77ad86d62084c5d5b52bdb"
-};
+// your firebase config code
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
